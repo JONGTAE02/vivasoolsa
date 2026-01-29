cd842fd83b7a1703c0751409d831c69f70fcc7e0
JONGTAE02 <qwx0677@naver.com>
2026-01-29 09:48:56 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/org/example/visang_vivasoolsa/VisangVivaSoolsaApplication.java
@@ -0,0 +1,13 @@
+package org.example.visang_vivasoolsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisangVivaSoolsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisangVivaSoolsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/visang_vivasoolsa/VisangVivaSoolsaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.visang_vivasoolsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisangVivaSoolsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
